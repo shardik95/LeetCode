ed90f5b91e2e69c887fdbbe50496d63e15fd4e0b
shardik95 <shardik95@ccs.neu.edu>
2018-08-15 23:56:57 -0400

    Question 76

---
+++ 76-WindowSubstring.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character,Integer> map = new HashMap<>();
+        int end = 0;
+        int begin = 0;
+        int min = Integer.MAX_VALUE;
+        int count = t.length();
+        int head = 0;
+        for(int i=0;i<t.length();i++){
+            char x = t.charAt(i);
+            if(map.containsKey(x))
+                map.put(x,map.get(x)+1);
+            else map.put(x,1);
+        }
+        while(end<s.length()){
+            char x = s.charAt(end);
+            if(map.containsKey(x)){
+                 if(map.get(x)>0) count--;
+                 map.put(x,map.get(x)-1);
+            }
+            end++;
+            if(count ==0){
+                if(min > end - begin){
+                    min = end - begin;
+                    head = begin;
+                }
+            }
+            char first = s.charAt(begin);
+            if(map.containsKey(first)){
+                map.put(first,map.get(first)+1);
+                if(map.get(first)>0) count++;
+            }
+            begin++;
+        }
+        return min==Integer.MAX_VALUE? "":s.substring(head,head+min);
+    }
+}
