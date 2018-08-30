13c21b895eedee9a06545f88a9a4ceefbf9ee484
shardik95 <shardik95@ccs.neu.edu>
2018-08-30 17:13:29 +0530

    Question 205

---
+++ 205-IsomorphicStrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map = new HashMap<>();
+        Map<Character,Character> map2 = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i))!=t.charAt(i))
+                    return false;
+            }
+            else {
+               if(map2.containsKey(t.charAt(i)))
+                   return false;
+                map.put(s.charAt(i),t.charAt(i));
+                map2.put(t.charAt(i),s.charAt(i));
+            }
+        }
+        return true;
+    }
+}
